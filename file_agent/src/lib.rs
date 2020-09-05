8ffef03dd2d51230dcb2f8874b3b74b20cf5e096
Hongbin Mao <hello2mao@gmail.com>
2020-09-05 16:39:02 +0800

    Lazy push: 2020年 9月 5日 星期六 16时39分02秒 CST

--- file_agent/src/lib.rs
+++ file_agent/src/lib.rs
@@ -1,8 +1,6 @@
 #[macro_use]
 extern crate log;
 
-use serde::{Deserialize, Deserializer, Serialize};
-
 mod types;
 pub use types::{FileAgentRequest, HandleFileCommand, HandleFileInfo};
 mod agent;
