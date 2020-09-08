af6e7a47749675cf3b72711a5555d28d4b3f61b9
Hongbin Mao <hello2mao@gmail.com>
2020-09-08 10:53:35 +0800

    Lazy push: 2020年 9月 8日 星期二 10时53分35秒 CST

---
+++ enum_demo/src/main.rs
@@ -0,0 +1,40 @@
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move{x: i32, y: i32},
+    Write(String),
+    Change(i32, i32, i32),
+}
+
+impl Message {
+    fn print(&self) {
+        match *self {
+            Message::Quit => println!("Quit"),
+            Message::Move{x, y} => println!("Move x = {}, y = {}", x, y),
+            Message::Change(a, b, c) => println!("Change a = {}, b = {}, c = {}", a, b, c),
+            _ => println!("Write"),
+        }
+    }
+}
+
+fn main() {
+
+
+    let msg1 = Message::Quit;
+    msg1.print();
+    println!("msg1: {:#?}", msg1);
+
+    let msg2 = Message::Move{
+        x: 1,
+        y: 2,
+    };
+    msg2.print();
+    println!("msg2: {:#?}", msg2);
+
+    let msg3 = Message::Change(1,2,3);
+    msg3.print();
+
+
+    println!("Hello, world!");
+}
