3dfb36c486dd615c8f4f21f838d65e7c1a622739
Hongbin Mao <hello2mao@gmail.com>
2020-09-16 17:50:16 +0800

    Lazy push: 2020年 9月16日 星期三 17时50分16秒 CST

---
+++ rc_demo/src/main.rs
@@ -0,0 +1,16 @@
+//通过Rc<T>允许程序的多个部分之间只读的共享数据。
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+fn main() {
+    let a= Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
+
+    println!("Hello, world!");
+}
